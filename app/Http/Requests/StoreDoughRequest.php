33492dbc4b518e83bd959b9f2b2d4e8078ea34f8
Jack Ellis <jackellis1504@gmail.com>
2025-09-18 21:50:27 +0100

    added dough, updated stubs
     M stubs/policy.stub
     M stubs/request.stub
    ?? app/Http/Controllers/DoughController.php
    ?? app/Http/Requests/
    ?? app/Models/Dough.php
    ?? app/Policies/
    ?? database/factories/DoughFactory.php
    ?? database/migrations/2025_09_18_204913_create_doughs_table.php
    ?? database/seeders/DoughSeeder.php

--- app/Http/Requests/StoreDoughRequest.php
+++ app/Http/Requests/StoreDoughRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace {{ namespace }};
+namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class {{ class }} extends FormRequest
+class StoreDoughRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
